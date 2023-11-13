1d99ed635f1fe566396d09723e4f2624e8908d64
Jason Bright <jason.bright@horne.com>
2023-11-12 21:17:08 -0600

    Setup Connection String; Add Unit Tests; ToDo: Abstract connection string method

--- WinFormMiniProject/Program.cs
+++ WinFormMiniProject/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,9 +16,25 @@ namespace WinFormMiniProject
         [STAThread]
         static void Main()
         {
+            GetConnectionString();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PersonEntry());
+        }
+
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
         }
     }
 }
